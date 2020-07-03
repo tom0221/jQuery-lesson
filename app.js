4874a3b3f303571b0be3f118566b31a4395a9558
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-03 19:05:39 +0900

    [finish]第7章まで

--- app.js
+++ app.js
@@ -9,11 +9,35 @@
 // 	});
 // });
 
+// $(function(){
+// 	$('.box1').on('click', function(){
+// 		$('.box1').addClass('box1-ext');
+// 	});
+// 	$('.box1').mouseout(function(){
+// 		$('.box1').removeClass('box1-ext');
+// 	});
+// });
+
+// $(function(){
+// 	$('.bg1').on('click', function(){
+// 		$('.bg1').slideUp();
+// 	});
+
+// 	$('.bg2').on('click', function(){
+// 		$('.bg2').slideUp();
+// 	});
+
+// 	$('.bg3').on('click', function(){
+// 		$('.bg3').slideUp();
+// 	});
+
+// 	$('.bg4').on('click', function(){
+// 		$('.bg4').slideUp();
+// 	});
+// });
+
 $(function(){
-	$('.box1').mouseover(function(){
-		$('.box1').css({'background-color': '#0000FF'});
-	});
-	$('.box1').mouseout(function(){
-		$('.box1').css({'background-color': '#FF0000'});
+	$('button').on('click', function(){
+		$('ul').children().css('color', 'red');
 	});
 });
